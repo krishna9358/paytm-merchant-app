957951554e7e726407758e65d0151b401255775f
Krishna <krishanmohank974@gmail.com>
2024-07-27 20:54:08 +0530

    added prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { userRouter } from "./router/user";
+import {merchantRouter} from "./router/merchant"
+
 
 const app = express();
 
 app.use("/api/v1/user", userRouter)
+app.use("/api/v1/user", merchantRouter)
+
 
 app.listen(3000);
---
+++ backend/src/router/merchant.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+export const merchantRouter = Router();
+
+merchantRouter.post("/signup", (req,res) => {
+
+
+})
+
+merchantRouter.post("/signin", (req, res) => {
+
+
+})
